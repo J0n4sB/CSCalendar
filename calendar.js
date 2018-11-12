8d8c84622bfcfcb8f6cc1a9025a088f0a3c77fe0
J0n4sB <44502683+J0n4sB@users.noreply.github.com>
2018-11-12 13:51:14 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,26 +1,34 @@
 
 
 
-
-var selector, elems, makeActive;
-
-selector = '.days li';
-
-elems = document.querySelectorAll(selector);
-
-makeActive = function () {
-    for (var i = 0; i < elems.length; i++)
-        elems[i].classList.remove('active');
-    
-    this.classList.add('active');
-};
-
-for (var i = 0; i < elems.length; i++)
-    elems[i].addEventListener('mousedown', makeActive);
+var utc = new Date().toJSON().slice(8,10).replace(/-/g,'/');
 
 
 
-document.getElementById("demo").innerHTML = 'JavaScript = Working';
+
+//document.getElementById("demo").innerHTML = utc;
+
+function dateCheck(){
+
+    var i;
+    for (i = 0; i <30; i++){
+        if(document.getElementById('P' + i).innerHTML == utc){
+            document.getElementById("P" +i).style.color = 'white';
+        }
+    }
+
+
+/*    var x = document.getElementsByTagName("li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        //document.getElementById("demo").innerHTML = x.length;
+    if (x[i].innerHTML == utc){
+      document.getElementById("demo").innerHTML = x[i].innerHTML;
+      x[i].style.color = "white";
+  }
+}*/
+
+}
 
 
 
@@ -28,4 +36,51 @@ function eventVisible(){
     document.getElementById("myModal").style.opacity = "1";
     document.getElementById("myModal").style.zIndex = "0";
 }
+
+
+
+
+
+
+
+function createDiv(){
+
+var i;
+for(i = 0; i <30; i++){
+
+
+var div = document.createElement("div");
+div.id = i;
+div.style.backgroundColor= "red";
+div.style.width= "14%";
+div.style.height= "180px";
+div.style.textAlign= "left";
+div.style.float= "left";
+div.style.border= "solid";
+div.style.borderColor= "black";
+
+
+
+
+//div.innerHTML = i;
+
+document.getElementById("main").appendChild(div);
+
+    var para = document.createElement("P");
+    para.id = 'P'+i;
+    var t = document.createTextNode(i);
+    para.appendChild(t);
+    document.getElementById(i).appendChild(para);
+
+
+}
+
+
+
+
+}
+
+
+
+
 
